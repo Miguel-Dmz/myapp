01488923ff22a6850aa9d155e198670580a27aee
a.22308051281173 <a.22308051281173@cbtis128.edu.mx>
2025-05-20 15:28:13 +0000

    act1

---
+++ lib/main.dart
@@ -0,0 +1,220 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(fontFamily: 'Nunito'),
+    home: HomePage(),
+  ),
+);
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late PageController _pageController;
+  int totalPage = 4;
+
+  void _onScroll() {}
+
+  @override
+  void initState() {
+    _pageController = PageController(initialPage: 0)..addListener(_onScroll);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        children: <Widget>[
+          makePage(
+            page: 1,
+            image: 'assets/jordan_1.jpg',
+            title: 'Air Jordan 1 Mid Black Iron Grey',
+            description:
+                'jordan 1 aca pasados de lanza no apto para desconocedores',
+          ),
+          makePage(
+            page: 2,
+            image: 'assets/jordan_4.jpg',
+            title: 'Las Air Jordan 4 "Fire Red" listas para el Black Friday',
+            description: 'jordan  4 rojitos aca bn rojos pero muy muy rojos',
+          ),
+          makePage(
+            page: 3,
+            image: 'assets/jordan_4_Travis.jpg',
+            title: 'Travis Scott x Air Jordan 4 Houston Oilers Cactus Jack',
+            description:
+                "jordan 4 del travieso muy clean y una belleza no aptan para desconocedores.",
+          ),
+          makePage(
+            page: 4,
+            image: 'assets/jordan_1_retro.jpg',
+            title: 'Jordan Air 1 Retro High',
+            description:
+                "jordan 1 retro pero muy retros aca de los años 1750 cuando tu abuelo apenas le tiraba el pedo a tu abuela+{-}.",
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget makePage({image, title, description, page}) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomRight,
+            stops: [0.3, 0.9],
+            colors: [
+              Colors.black.withOpacity(.9),
+              Colors.black.withOpacity(.2),
+            ],
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.baseline,
+                textBaseline: TextBaseline.alphabetic,
+                children: <Widget>[
+                  FadeInUp(
+                    child: Text(
+                      page.toString(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    '/' + totalPage.toString(),
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    FadeInUp(
+                      child: Text(
+                        title,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 50,
+                          height: 1.2,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    FadeInUp(
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.only(right: 3),
+                            child: Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                              size: 15,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 3),
+                            child: Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                              size: 15,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 3),
+                            child: Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                              size: 15,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 3),
+                            child: Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                              size: 15,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 5),
+                            child: Icon(
+                              Icons.star,
+                              color: Colors.grey,
+                              size: 15,
+                            ),
+                          ),
+                          Text('4.0', style: TextStyle(color: Colors.white70)),
+                          Text(
+                            '(2300)',
+                            style: TextStyle(
+                              color: Colors.white38,
+                              fontSize: 12,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    FadeInUp(
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 50),
+                        child: Text(
+                          description,
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(.7),
+                            height: 1.9,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    FadeInUp(
+                      child: Text(
+                        'READ MORE',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
